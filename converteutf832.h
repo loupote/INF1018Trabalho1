4c0778b465495e48272ef76ad0c85b9845d9127c
Louis Pottier <167591444+loupote@users.noreply.github.com>
2024-10-06 18:25:15 -0300

    Delete converteutf832.h

--- converteutf832.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-/* UTF-8  para UTF-32 */
-
-int convUtf8p32(FILE *arquivo_entrada, FILE *arquivo_saida);
-unsigned int convbigtolittle(unsigned int codepoint, unsigned int bytes);
-
-
-/* UTF-32 para UTF-8 */
